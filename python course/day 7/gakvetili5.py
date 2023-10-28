4292327d4138d7ac91a2f5acd5e213dc4021661a
Nikashengelia <113446914+Nikashengelia@users.noreply.github.com>
2023-10-28 19:16:17 +0400

    Add files via upload

---
+++ python course/day 7/gakvetili5.py
@@ -0,0 +1,8 @@
+salary = int(input("What is your salary?"))
+if salary > 10000:
+    print("Did you study at GOA?")
+elif 1000 <= salary <= 10000: 
+    print("You are Mid")
+
+else:
+    print("Why didn't you join GOA?")
