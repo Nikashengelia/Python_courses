cffcbc1e872b7831cc9c849baa0edd60e35b7ea7
Nikashengelia <113446914+Nikashengelia@users.noreply.github.com>
2023-10-29 20:02:55 +0400

    Add files via upload

---
+++ python course/lesson6/gakvetili6.py
@@ -0,0 +1,29 @@
+number = 1
+while number <= 30:
+    if number % 2 == 0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
+    number += 1
+
+
+
+###### meore davaleba
+
+daily_routine = [
+    "7:00 AM - Wake up and get ready",
+    "7:30 AM - Breakfast",
+    "8:00 AM - Exercise",
+    "9:00 AM - school",
+    "12:00 PM - Lunch",
+    "1:00 PM -  Continue school",
+    "3:00 PM - Finish  school",
+    "5:00 PM - Coding"
+    "6:00 PM - Dinner",
+    "7:00 PM - Coding",
+    "9:00 PM -Bath",
+    "10:00 PM - Bedtime"
+]
+
+for activity in daily_routine:
+    print(activity)
