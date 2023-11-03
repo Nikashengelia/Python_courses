89c12b1974e68a0bae668b1a644b2be1cd34cc14
Nikashengelia <113446914+Nikashengelia@users.noreply.github.com>
2023-11-03 21:26:49 +0400

    Add files via upload

---
+++ python course/day 12/gakvetili7.py
@@ -0,0 +1,8 @@
+ticket = 25  
+adults = 10
+childrens = 3
+total_cost_adults = adults * ticket
+total_cost_children = childrens * 0  
+total_cost = total_cost_adults + total_cost_children
+
+print(f"The total cost for the group is {total_cost} GEL.")
